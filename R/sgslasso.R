0061aab82e0d2cbd70aff22ac29a3759c3d53080
Tanya Garcia <37775239+tpgarcia@users.noreply.github.com>
2020-03-01 00:44:31 -0600

    Deleted hello.R files. Added sgslasso.R file

---
+++ R/sgslasso.R
@@ -0,0 +1,15 @@
+#' sgslasso package
+#'
+#' This package implements the sparse group-subgroup lasso which
+#' identifies important regressor groups, subgroups, and individiual
+#' variables.
+#'
+#' @references
+#'
+#' Garcia, T.P., Müller, S., Carroll, R.J., and Walzem, R.L. (2013).
+#' Identification of important regressor groups, subgroups, and individuals via regularization methods: application to gut
+#' microbiome data. Bioinformatics, DOI: 10.1093/bioinformatics/btt608.
+#'
+#' @docType package
+#' @name sgslasso
+NULL
